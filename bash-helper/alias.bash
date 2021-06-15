6bf50570b4eb01f7196cd31d492aac34659db573
Andrew Fishberg <fishberg.dev@gmail.com>
2021-06-15 17:26:23 -0400

    added bash-helper

---
+++ bash-helper/alias.bash
@@ -0,0 +1,7 @@
+alias src='source ~/.bashrc'
+alias vi=nvim
+
+alias ls='ls --color'
+alias l='ls'
+alias ll='ls -l'
+alias la='ls -a'
---
+++ bash-helper/ps1.bash
@@ -0,0 +1,21 @@
+# foreground color
+BLACK='\[\033[30m\]'
+RED='\[\033[31m\]'
+GREEN='\[\033[32m\]'
+YELLOW='\[\033[33m\]'
+BLUE='\[\033[34m\]'
+PURPLE='\[\033[35m\]'
+CYAN='\[\033[36m\]'
+WHITE='\[\033[37m\]'
+
+# background color
+BG_BLACK='\[\033[40m\]'
+BG_RED='\[\033[41m\]'
+BG_GREEN='\[\033[42m\]'
+BG_YELLOW='\[\033[43m\]'
+BG_BLUE='\[\033[44m\]'
+BG_PURPLE='\[\033[45m\]'
+BG_CYAN='\[\033[46m\]'
+BG_WHITE='\[\033[47m\]'
+
+export PS1="\n$GREEN\u@\h$WHITE:$YELLOW\w$WHITE\n$ "
