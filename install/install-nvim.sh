e43db0195cff8d4a80f04667069f1bc0bacbfb4f
Andrew Fishberg <fishberg.dev@gmail.com>
2021-06-30 11:50:43 -0400

    added referene for running :PlugInstall from cli

--- install/install-nvim.sh
+++ install/install-nvim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install VimPlug
-# https://github.com/junegunn/vim-plug
+## https://github.com/junegunn/vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
@@ -10,4 +10,6 @@ mkdir -p ~/.config/nvim
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
 # run :PlugInstall
+## How to run PlugInstall directly from command line
+## https://github.com/junegunn/vim-plug/issues/675
 nvim +'PlugInstall --sync' +qa
