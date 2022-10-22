29c3bc71b3f57c9952758c1587bd2e44a4910bea
Andrew Fishberg <fishberg.dev@gmail.com>
2022-10-22 15:51:26 -0400

    added tunnel bash command

--- bash/global/functions.bash
+++ bash/global/functions.bash
@@ -1,3 +1,8 @@
 autopy(){
     eval "$(register-python-argcomplete $1)"
 }
+
+# https://superuser.com/questions/62303/how-can-i-tunnel-all-of-my-network-traffic-through-ssh
+tunnel(){
+    sshuttle -r $1 0.0.0.0/0 -vv
+}
