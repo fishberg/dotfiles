72a10e2d944ea4e3c91b7162f4b4a79118003de7
Andrew Fishberg <fishberg.dev@gmail.com>
2023-02-17 14:37:34 -0500

    added home, downloads, dotfiles, and fixed lastDL

--- bash/global/alias.bash
+++ bash/global/alias.bash
@@ -4,6 +4,11 @@ alias copy='xclip -selection c'
 alias strip='nbstripout *.ipynb'
 alias exe='chmod +x'
 alias open='nohup nautilus -w . >/dev/null 2>&1'
+
+alias home='cd ~'
+alias downloads='cd ~/Downloads'
+
+alias dotfiles='cd ~/.dotfiles'
 alias dotfiles_sync='git -C ~/.dotfiles pull; src'
 
 alias ls='ls --color'
@@ -15,6 +20,6 @@ alias lt='ls -t'
 
 alias devel='source devel/setup.bash'
 
-alias lastDL='ls -t ~/Downloads | egrep -v "\.part$" | head -1 | xargs find ~/Downloads -name'
+alias lastDL='ls -t ~/Downloads | egrep -v "\.part$" | head -1 | xargs -d '\n' find ~/Downloads -name'
 
 alias today='date "+%F"'
