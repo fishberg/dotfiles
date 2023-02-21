203d3ada68322376f1ad08f887006ed74ef1715c
Andrew Fishberg <fishberg.dev@gmail.com>
2023-02-20 22:11:36 -0500

    fixed convert-to-ssh.bash

--- install/dotfiles-repo/convert-to-ssh.bash
+++ install/dotfiles-repo/convert-to-ssh.bash
@@ -2,4 +2,4 @@
 
 # https://www.howtogeek.com/devops/how-to-switch-a-github-repository-to-ssh-authentication/
 git -C ~/.dotfiles remote rm origin
-git -C ~/.dotfiles add origin git@github.com:fishberg/dotfiles
+git -C ~/.dotfiles remote add origin git@github.com:fishberg/dotfiles
