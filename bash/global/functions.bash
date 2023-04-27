e507abae6c21e90beb7ca98aec30f6c488dc3c50
Andrew Fishberg <fishberg.dev@gmail.com>
2023-04-27 14:55:49 -0400

    added readme_init

--- bash/global/functions.bash
+++ bash/global/functions.bash
@@ -67,3 +67,11 @@ sound_test(){
 sound_test2(){
     hexdump -e '/1 "%u " /1 "%u\n"' /dev/urandom | awk '{ split("0,2,4,5,7,8,10,12",a,",");split("0.25,0.25,0.25,0.5,0.5,1",b,",");for (i = 0; i < b[$2 %7]; i+= 0.0001) printf("%08X\n", 100*sin(1382*2**(a[$1 %9]/12)*i)) }' | xxd -r -p | aplay -c 2 -f S32_LE -r 16000
 }
+
+readme_init(){
+    if [[ ! -e README.md ]]; then
+        echo "# $(basename $(pwd))" > README.md
+    else
+        echo "README.md already exists, skipping..."
+    fi
+}
