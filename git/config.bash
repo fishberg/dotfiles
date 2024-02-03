200d902477aeb758793ab4be1bcf52f6661e5774
Andrew Fishberg <fishberg.dev@gmail.com>
2024-02-03 12:11:51 -0500

    remove git folder, since config is in install

--- git/config.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global user.name "Andrew Fishberg"
-git config --global user.email "fishberg.dev@gmail.com"
-
-# https://stackoverflow.com/questions/13846300/how-to-make-git-pull-use-rebase-by-default-for-all-my-repositories
-git config --global pull.rebase true
