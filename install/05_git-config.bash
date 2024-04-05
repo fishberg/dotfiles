3f14457afc167a5e23aa9afa8f90f9e692cc8e68
Andrew Fishberg <fishberg.dev@gmail.com>
2024-04-05 18:06:53 -0400

    git default main branch

--- install/05_git-config.bash
+++ install/05_git-config.bash
@@ -7,5 +7,8 @@ git config --global user.email "fishberg.dev@gmail.com"
 # use vim
 git config --global core.editor nvim
 
+# set default branch
+git config --global init.defaultBranch main
+
 # rebase instead of merge
 git config --global pull.rebase true
