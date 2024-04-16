829190485c37ea5eddd4f57518cfdfce0665959d
Andrew Fishberg <fishberg.dev@gmail.com>
2024-04-16 17:21:17 -0400

    added comments to docker install

--- install/06_ubuntu-20.04_docker-install.bash
+++ install/06_ubuntu-20.04_docker-install.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
+# https://docs.docker.com/engine/install/ubuntu/
 
 # install docker
 sudo apt update
@@ -9,6 +10,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install -y docker-ce
 
+# https://docs.docker.com/engine/install/linux-postinstall/
 # docker doesn't require sudo
 sudo usermod -aG docker ${USER}
 su - ${USER}
