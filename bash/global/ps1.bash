36239991018a06c71e6dbb42d03ea993928cd975
Andrew Fishberg <fishberg.dev@gmail.com>
2024-10-04 19:40:47 -0400

    added RESET to PS1

--- bash/global/ps1.bash
+++ bash/global/ps1.bash
@@ -1,3 +1,5 @@
+# https://www.cyberciti.biz/faq/bash-shell-change-the-color-of-my-shell-prompt-under-linux-or-unix/
+
 # foreground color
 BLACK='\[\033[30m\]'
 RED='\[\033[31m\]'
@@ -18,10 +20,37 @@ BG_PURPLE='\[\033[45m\]'
 BG_CYAN='\[\033[46m\]'
 BG_WHITE='\[\033[47m\]'
 
+#RESET='\e(B\e[m'
+#BOLD='\[\033[1m\]'
+#BOLD='\e[1m'
+
+# foreground color
+#BLACK=$(tput setaf 0)
+#RED=$(tput setaf 1)
+#GREEN=$(tput setaf 2)
+#YELLOW=$(tput setaf 3)
+#BLUE=$(tput setaf 4)
+#PURPLE=$(tput setaf 5)
+#CYAN=$(tput setaf 6)
+#WHITE=$(tput setaf 7)
+
+# background color
+#BG_BLACK=$(tput setab 0)
+#BG_RED=$(tput setab 1)
+#BG_GREEN=$(tput setab 2)
+#BG_YELLOW=$(tput setab 3)
+#BG_BLUE=$(tput setab 4)
+#BG_PURPLE=$(tput setab 5)
+#BG_CYAN=$(tput setab 6)
+#BG_WHITE=$(tput setab 7)
+
+RESET=$(tput sgr0)
+BOLD=$(tput bold)
+
 # no git prompt
 #export PS1="\n$GREEN\u@\h$WHITE:$YELLOW\w$WHITE\n$ "
 
 # git prompt
 # combines ' and " strings so __git_ps1 reevals for each prompt
 export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1="\n$GREEN\u@\h$WHITE:$YELLOW\w$CYAN"'$(__git_ps1)'"$WHITE\n$ "
+export PS1="\n$GREEN\u@\h$WHITE:$YELLOW\w$CYAN"'$(__git_ps1)'"$WHITE\n$ $RESET"
