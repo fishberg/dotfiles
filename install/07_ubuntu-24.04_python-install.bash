0a896bb9cd0aab26459f4ada63055490afce3313
Andrew Fishberg <fishberg.dev@gmail.com>
2025-02-06 09:40:16 -0500

    changed python to install with apt

--- install/07_ubuntu-24.04_python-install.bash
+++ install/07_ubuntu-24.04_python-install.bash
@@ -4,18 +4,21 @@
 set -x
 
 # python3
-sudo apt install -y python-is-python3 python3-pip
+sudo apt install -y python-is-python3 python3-pip python3-venv
 
-# allow pip to install without venv
-sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/disabled_EXTERNALLY-MANAGED
+# system install
+sudo apt install python3-numpy python3-matplotlib python3-pandas python3-scipy python3-jupyterlab python3-tabulate python3-termcolor
 
-pip install numpy
-pip install matplotlib
-pip install pandas
-pip install scipy
-pip install jupyterlab
-pip install nbstripout
-pip install tabulate
-pip install termcolor
-pip install grip
-pip install tmuxp
+## allow pip to install without venv
+#sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/disabled_EXTERNALLY-MANAGED
+#
+#pip install numpy
+#pip install matplotlib
+#pip install pandas
+#pip install scipy
+#pip install jupyterlab
+#pip install nbstripout
+#pip install tabulate
+#pip install termcolor
+#pip install grip
+#pip install tmuxp
