4c50412e0dd977d001d881f33f54c1adabf5c759
Andrew Fishberg <fishberg.dev@gmail.com>
2025-02-09 13:16:20 -0500

    added pipx

--- install/07_ubuntu-24.04_python-install.bash
+++ install/07_ubuntu-24.04_python-install.bash
@@ -9,6 +9,13 @@ sudo apt install -y python-is-python3 python3-pip python3-venv
 # system install
 sudo apt install python3-numpy python3-matplotlib python3-pandas python3-scipy python3-jupyterlab python3-tabulate python3-termcolor
 
+# install pipx
+sudo apt install pipx
+
+pipx install jupyterlab
+pipx install nbstripout
+pipx install grip
+
 ## allow pip to install without venv
 #sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/disabled_EXTERNALLY-MANAGED
 #
