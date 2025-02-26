47164279e65b0c3d8a749b93a610f3afdd395c48
Andrew Fishberg <fishberg.dev@gmail.com>
2025-02-25 19:25:49 -0500

    switch inkscape to flatpak

--- install/24_media-install.bash
+++ install/24_media-install.bash
@@ -8,9 +8,13 @@ sudo apt install vlc
 sudo apt install audacity
 sudo snap install gimp
 sudo snap install kdenlive
-sudo snap install inkscape
 
 # OBS
 # https://flathub.org/apps/com.obsproject.Studio
 sudo flatpak install flathub com.obsproject.Studio
 # flatpak run com.obsproject.Studio
+
+# inkscape
+# https://flathub.org/apps/org.inkscape.Inkscape
+flatpak install flathub org.inkscape.Inkscape
+# flatpak run org.inkscape.Inkscape
