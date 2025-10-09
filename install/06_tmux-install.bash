be5af1d2285f63af868841486e63aba3bfeba541
Andrew Fishberg <fishberg.dev@gmail.com>
2025-10-09 12:08:21 -0400

    updated tmux config and install

--- install/06_tmux-install.bash
+++ install/06_tmux-install.bash
@@ -3,4 +3,8 @@
 # show commands
 set -x
 
+# link config file
 ln -s $DOTFILES_ROOT/tmux/_tmux.conf ~/.tmux.conf
+
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
