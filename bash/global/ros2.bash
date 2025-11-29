b274610c3f4595a726fede329c25d63307edbe49
Andrew Fishberg <fishberg.dev@gmail.com>
2025-11-29 12:25:10 -0500

    added ROS2 shortcuts

--- bash/global/ros2.bash
+++ bash/global/ros2.bash
@@ -1,3 +1,16 @@
 roscd() {
     cd $(ros2 pkg prefix --share $1)
 }
+
+rsrc() {
+    source ./install/setup.bash
+}
+
+# create cpp package
+# ros2 pkg create my_package --build-type ament_cmake --dependencies rclcpp std_msgs
+
+# create python package
+# ros2 pkg create my_py_pkg --build-type ament_python --dependencies rclpy std_msgs
+
+# build with symlink
+# colcon build --symlink-install
