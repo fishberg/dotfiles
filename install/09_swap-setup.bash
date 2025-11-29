5eac801e9fe030ce00c52b8d29fd8b76bc058f24
Andrew Fishberg <fishberg.dev@gmail.com>
2025-11-29 12:31:09 -0500

    updated swap install script to support swap.img and various sizes

--- install/09_swap-setup.bash
+++ install/09_swap-setup.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+########################################
+# CONFIG
+########################################
+
+#SWAPFILE='/swapfile' # pre-24.04
+SWAPFILE='/swap.img'  # 24.04+
+SIZE_GiB=16
+
+########################################
+
 # https://stackoverflow.com/questions/3231804/in-bash-how-to-add-are-you-sure-y-n-to-any-command-or-alias
 confirm() {
     # ${VAR:-default} uses VAR value if not null, and the default value if null
@@ -16,6 +26,8 @@ confirm() {
     esac
 }
 
+SIZE_MiB=$(echo "$SIZE_GiB * 1024" | bc)
+
 ########################################
 
 # EUID == 0 if root user
@@ -28,7 +40,7 @@ fi
 # check current swap status
 swapon -s
 
-if ! confirm "Showing current swapfiles. Will detach all swapfiles and create new /swapfile (16GB). Continue?"; then
+if ! confirm "Showing current swapfiles. Will detach all swapfiles and create new $SWAPFILE (${SIZE_GiB}GiB). Continue?"; then
     echo "Exiting"
     exit
 fi
@@ -40,25 +52,24 @@ set -x
 # remove all swaps
 swapoff -a
 
-# create swap file (16GB)
-dd if=/dev/zero of=/swapfile bs=1M count=16384 status=progress
+# create swap file
+dd if=/dev/zero of=$SWAPFILE bs=1M count=$SIZE_MiB status=progress
 
 # setup swap file
-mkswap /swapfile
-chmod 0600 /swapfile
+mkswap $SWAPFILE
+chmod 0600 $SWAPFILE
 
 # attach swap
-swapon /swapfile
+swapon $SWAPFILE
 
 # Unsets show commands as they run
 set +x
 
 # add to /etc/fstab if needed
-FSTAB_LINE='/swapfile                                 none            swap    sw              0       0'
-if [[ $(grep -e '^/swapfile' /etc/fstab | wc -l) == 0 ]]; then
+FSTAB_LINE="$SWAPFILE none swap sw 0 0"
+if [[ $(grep -e "^$SWAPFILE" /etc/fstab | wc -l) == 0 ]]; then
     echo "adding line to /etc/fstab"
     echo $FSTAB_LINE >> /etc/fstab
 else
-    echo "not adding line to /etc/fstab, detected prexisting /swapfile line"
+    echo "not adding line to /etc/fstab, detected prexisting $SWAPFILE line"
 fi
-
