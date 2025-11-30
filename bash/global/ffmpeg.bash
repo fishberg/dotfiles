cdb79d4b9e121d95c4ace02a6a087b3a68b1120a
Andrew Fishberg <fishberg.dev@gmail.com>
2025-11-30 18:21:34 -0500

    added ffmpeg shortcuts

--- bash/global/ffmpeg.bash
+++ bash/global/ffmpeg.bash
@@ -8,3 +8,36 @@ ffmpeg_mute() {
     ffmpeg -i $1 -c copy -an $2
     set +x
 }
+
+ffmpeg_rot_cw90() {
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: ${FUNCNAME[0]} <infile> <outfile>" >&2
+        return 1
+    fi
+
+    set -x
+    ffmpeg -i $1 -vf "transpose=1" $2
+    set +x
+}
+
+ffmpeg_rot_ccw90() {
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: ${FUNCNAME[0]} <infile> <outfile>" >&2
+        return 1
+    fi
+
+    set -x
+    ffmpeg -i $1 -vf "transpose=2" $2
+    set +x
+}
+
+ffmpeg_rot_180() {
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: ${FUNCNAME[0]} <infile> <outfile>" >&2
+        return 1
+    fi
+
+    set -x
+    ffmpeg -i $1 -vf "hflip,vflip" $2
+    set +x
+}
