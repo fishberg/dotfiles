551f2d03bd6ff088fe50332158bd2a29f3b6d0da
Andrew Fishberg <fishberg.dev@gmail.com>
2025-12-02 08:55:55 -0500

    added shortcuts for ffmpeg audio extract

--- bash/global/ffmpeg.bash
+++ bash/global/ffmpeg.bash
@@ -9,6 +9,30 @@ ffmpeg_mute() {
     set +x
 }
 
+ffmpeg_audio_extract_noreencode() {
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: ${FUNCNAME[0]} <infile> <outfile>" >&2
+        echo "To extract without reincoding, the output container must support the codec inside the video"
+        echo "e.g., mp4 -> m4a"
+        return 1
+    fi
+
+    set -x
+    ffmpeg -i $1 -vn -acodec copy $2
+    set +x
+}
+
+ffmpeg_audio_extract_mp3() {
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: ${FUNCNAME[0]} <infile> <outfile>" >&2
+        return 1
+    fi
+
+    set -x
+    ffmpeg -i $1 -vn -acodec libmp3lame -q:a 2 $2
+    set +x
+}
+
 ffmpeg_rot_cw90() {
     if [ "$#" -ne 2 ]; then
         echo "Usage: ${FUNCNAME[0]} <infile> <outfile>" >&2
