7f6e97f65a483388932de9e2efe48b6c39af1aad
Andrew Fishberg <fishberg.dev@gmail.com>
2026-03-05 11:17:04 -0500

    added text_send

--- bash/global/misc.bash
+++ bash/global/misc.bash
@@ -2,3 +2,11 @@
 print256colors() {
     for i in {0..255}; do printf "\x1b[38;5;${i}mcolor${i} - ██████████\n"; done
 }
+
+text_send () {
+    local host="$1"
+    local file="$2"
+    shift 2
+
+    printf '%s\n' "$*" | ssh "$host" "cat >> '$file'"
+}
