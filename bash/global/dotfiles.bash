ea92293c3267ac5b92f59a9c6dea7e640a052075
Andrew Fishberg <fishberg.dev@gmail.com>
2026-03-11 01:10:11 -0400

    updated dotfiles bash aliases

--- bash/global/dotfiles.bash
+++ bash/global/dotfiles.bash
@@ -1,2 +1,7 @@
 alias dotfiles='cd $DOTFILES_ROOT'
 alias dotfiles_up='git -C $DOTFILES_ROOT pull; src'
+
+alias dotfiles_bash='cd $DOTFILES_ROOT/bash/global'
+alias dotfiles_bash_local='cd $DOTFILES_ROOT/bash/local/$HOSTNAME'
+
+alias dotfiles_install='cd $DOTFILES_ROOT/install'
